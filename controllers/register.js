e41aa3d7e97bccce578ca07d458a49e89e34286c
Akansha Tiwari <akanshat1999@gmail.com>
2020-03-28 19:45:08 +0530

    uodates

--- controllers/register.js
+++ controllers/register.js
@@ -5,7 +5,7 @@ const handleRegister = async (req, res, db) => {
   email = mail.toLowerCase()
 
   if (!name || !email || !password)
-    return res.status(400).json('All fields are required')
+    return res.status(400).json({error: 'All fields are required'})
 
   const checkUser = await db.collection('users').findOne({ email })
   if (checkUser) res.status(400).json({ error: 'Email already exists' })
@@ -15,7 +15,7 @@ const handleRegister = async (req, res, db) => {
       name,
       email,
       hash
-    })
+    });
     res.status(200).json({ message: 'Registered succesfully!' })
   }
 }
