854ca3f51c7968195d7958f74856b0d7ded8727e
Akansha Tiwari <akanshat1999@gmail.com>
2020-03-30 00:05:48 +0530

    updates

--- controllers/order.js
+++ controllers/order.js
@@ -1,10 +1,10 @@
 const order = async (req, res, db) => {
+    const { name, priceSelected } = req.body;
 
     const items = await db.collection("cart").find({}).toArray();
     await db.collection("cart").insertOne({
         name,
-        priceSelected,
-        directions
+        priceSelected
       });
       res.status(200).json({ message: 'Added to cart' })
     return res.status(200).json({items});
